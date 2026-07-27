4046fd6b04f35f540d07024a19ec8d334293c54c
tweet-digest <tweet-digest@localhost>
2026-07-27 19:11:47 +0800

    Dual trigger: startup + 18:10, time-gated to 18:00-18:59

--- daily_trigger.bat
+++ daily_trigger.bat
@@ -1,3 +1,7 @@
+@echo off
+:: Only push between 18:00-18:59
+set HOUR=%time:~0,2%
+if %HOUR% NEQ 18 exit /b 0
 cd /d "C:\Users\ming\Documents\Codex\2026-07-22\x\tweet-digest"
 git pull
 echo %date% %time% > trigger.txt
