00f188fd98e036169e6875b0c5dc68c767e5efdf
tweet-digest <tweet-digest@localhost>
2026-08-02 19:13:16 +0800

    Add proxy config to bat script

--- daily_trigger.bat
+++ daily_trigger.bat
@@ -1,11 +1,12 @@
 @echo off
-:: Only push between 18:00-18:59
 set HOUR=%time:~0,2%
 if %HOUR% NEQ 18 exit /b 0
+set HTTPS_PROXY=http://127.0.0.1:7897
+set HTTP_PROXY=http://127.0.0.1:7897
 cd /d "C:\Users\ming\Documents\Codex\2026-07-22\x\tweet-digest"
 git pull
 echo %date% %time% > trigger.txt
 git add trigger.txt
 git commit -m "Daily trigger %date%"
 git push
-echo Done - GitHub Actions will handle collection
+echo Done
